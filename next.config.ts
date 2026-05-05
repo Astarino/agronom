b20a41f68066bf44f25b7a9455cf828fcb1ef719
Astarino <jambekovabdullah@yandex.ru>
2026-05-05 10:57:15 +0300

    Remove deprecated instrumentationHook from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["grammy", "@prisma/client", "prisma"],
-  experimental: {
-    instrumentationHook: true,
-  },
   images: {
     remotePatterns: [{ protocol: "https", hostname: "**" }],
   },
